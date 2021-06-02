1a1f8df2c217f09f839b9ea23e01c6dc88a7baa2
Samuel Štancl <samuel.stancl@gmail.com>
2021-06-02 15:58:06 +0200

    fix $data type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -37,7 +37,7 @@ export interface Alpine {
 }
 export declare interface ComponentController {
     $el: AlpineElement;
-    $data: ProxyConstructor;
+    $data: any;
     $nextTickStack: CallableFunction[];
     $showDirectiveStack: any[];
     $watchers: {
